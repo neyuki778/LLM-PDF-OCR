9b89bd38baef7c514952384108e258be98ac2715
neyuki778 <2597605722@qq.com>
2026-01-25 14:51:00 +0800

    feat: 添加PDF拆分功能，支持指定输出目录和拆分跨度, 更新依赖

---
+++ pkg/pdf/split.go
@@ -0,0 +1,27 @@
+package pdf
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+)
+
+func SplitPDF(ctx context.Context, inputPath, outputDir string, span int) error {
+	if inputPath == "" || outputDir == "" {
+		return fmt.Errorf("Input/OutputPath should not be empty!")
+	} else if span <= 0 {
+		return  fmt.Errorf("Span should bigger than 0!")	
+	}
+
+	// 创建输出目录
+	_ = os.MkdirAll(outputDir, 0755)
+	
+	err := api.SplitFile(inputPath, outputDir, span, nil)
+	if err != nil {
+		fmt.Printf("拆分失败: %v\n", err)
+		return err
+	}
+	return err
+}
